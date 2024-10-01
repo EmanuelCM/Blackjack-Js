5b532cb6e2d8c6204650c9405df0cfbea1910830
emanuel chaupis muñoz <emanuelchaupismunoz@MacBook-Pro-de-emanuel.local>
2024-09-30 23:02:50 -0500

    Seccion 6-Refactorizado

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -6,170 +6,184 @@
  * 2S = Two of spades
  */
 
-let deck            = [];
-const tipos         = ['C','D','H','S'];
-const espedciales   = ['A','J','Q','K']
-let newDeck;
-let puntosJugador       =0;
-    puntosCPU   =0;
-let jugadorPerdio=false;
-let resultado;
+//! Funcion Anonima 
+// se usa para el patron modulo 
+const miModulo=(()=>{
 
-//Referencias HTML
-const btnPedir=document.querySelector('#btnPedir');
-const btnDetener=document.querySelector('#btnDetener');
-const btnNuevo=document.querySelector('#btnNuevo');
-const smallPuntos=document.querySelectorAll('small');
-const divCartaJugador=document.querySelector('#jugador-cartas') // #llamar por ID
-const divCartaCPU=document.querySelector('#computadora-cartas') // #llamar por ID
-// smallPuntosJugador.innerText=puntosJugador
-//Crea nuevo Deck
-const crearDeck=()=>{
-    for (let i=2;i<=10;i++){
-        for( let tipo of tipos ){
-            deck.push(i+tipo);
-        }
-    }
-    for(let especial of espedciales){
-        for (let tipo of tipos){
-            deck.push(especial+tipo);
+    "use strict"
 
-        }
-    }
-    return  deck=_.shuffle(deck);
-}
+    let deck            = [];
+    const tipos         = ['C','D','H','S'],
+          espedciales   = ['A','J','Q','K'];
+ 
 
-crearDeck();
-console.log(deck);
+    let puntosJugadores=[];
+
+    //Referencias HTML
+    const btnPedir=document.querySelector('#btnPedir'),
+          btnDetener=document.querySelector('#btnDetener'),
+          btnNuevo=document.querySelector('#btnNuevo');
 
 
-//Tomar una carta
-const pedirCarta=()=>{
+    const smallPuntos=document.querySelectorAll('small'),
+          divCartasJugadores=document.querySelectorAll('.divCartas')
+  
+    // smallPuntosJugador.innerText=puntosJugador
 
-        if(deck.length===0){
-            throw 'No hay mas cartas'
-        }else{
-        }
-        const carta=deck.pop();
-        
-
-        console.log(carta);
-        return carta;
-}
-
-const valorCarta=(carta)=>{
-
-const valor =carta.substring(0,carta.length-1);
+//Inicializar juego 
+const inicializarJuego=(numJugadores=2)=>{
+        deck= crearDeck();
     
- return(!isNaN(valor)) 
-    ? valor*1
-    : (valor==='A')
-        ?11
-        :10
+        puntosJugadores=[];
+
+        for(let i=0;i<numJugadores;i++){
+            puntosJugadores.push(0)
+        }
+        smallPuntos.forEach(elem=>elem.innerText=0);
+        divCartasJugadores.forEach(elem=>elem.innerHTML='');
+
+        btnDetener.disabled=false;
+        btnPedir.disabled=false;
+    
 }
 
-const turnoCPU=( puntosMinimos)=>{
+    //Crea nuevo Deck
+  const crearDeck=()=>{
+        deck=[];
+        for (let i=2;i<=10;i++){
+            for( let tipo of tipos ){
+                deck.push(i+tipo);
+            }
+        }
+        for(let especial of espedciales){
+            for (let tipo of tipos){
+                deck.push(especial+tipo);
+
+            }
+        }
+        return  _.shuffle(deck);
+    }
+
+ 
+
+    //Tomar una carta
+    const pedirCarta=()=>{
+
+            if(deck.length===0){
+                throw 'No hay mas cartas'
+            }
+            return deck.pop();
+    }
 
 
-        do{
-       
-            const carta=pedirCarta();
-            puntosCPU=puntosCPU+valorCarta(carta);
-            smallPuntos[1].innerText=puntosCPU;
+    const valorCarta=(carta)=>{
+
+    const valor =carta.substring(0,carta.length-1);
+        
+    return(!isNaN(valor)) 
+        ? valor*1
+        : (valor==='A')
+            ?11
+            :10
+    }
+
+
+    const acumularPuntos=(turno,carta)=>{
+        puntosJugadores[turno]=puntosJugadores[turno]+valorCarta(carta);
+        smallPuntos[turno].innerText=puntosJugadores[turno];
+        return puntosJugadores[turno];
+    }
+
+    const crearCarta=(carta,turno)=>{
+
+        const imgCarta=document.createElement('img'); //Creamos el elemento
+        imgCarta.src=`assets/cartas/${carta}.png`;
+        imgCarta.classList.add('carta'); //agregar la clase css
+        divCartasJugadores[turno].append(imgCarta);
+
+    }
+
+
+    const turnoCPU=( puntosMinimos)=>{
+        let puntosCPU=0;
             
-            const imgCarta=document.createElement('img'); //Creamos el elemento
-            imgCarta.src=`assets/cartas/${carta}.png`;
-            imgCarta.classList.add('carta'); //agregar la clase css
-            divCartaCPU.append(imgCarta);
+            do{
+                let turno=puntosJugadores.length-1;
+                const carta=pedirCarta();
+                puntosCPU =acumularPuntos(turno,carta);
+                crearCarta(carta,turno);
+                
+            }while((puntosMinimos>=puntosCPU)&&(puntosMinimos<=21));
 
-           
-         }while((puntosMinimos>=puntosCPU)&&(puntosMinimos<=21));
-    
-      isWiner();
+       
+            isWiner();
 
-
-
-
-}
+    }
 
 
-
-
-
-
-//! Eventos 
-// usamos el listener para escuchar al boton y hacemos un callback al mandar una funcion por el argumento 
-btnPedir.addEventListener('click',function(){
-    const carta=pedirCarta();
-    puntosJugador=puntosJugador+valorCarta(carta);
-    smallPuntos[0].innerText=puntosJugador;
-    
-    const imgCarta=document.createElement('img'); //Creamos el elemento
-    imgCarta.src=`assets/cartas/${carta}.png`;
-    imgCarta.classList.add('carta'); //agregar la clase css
-     divCartaJugador.append(imgCarta);
-
-     if(puntosJugador>21){
-         jugadorPerdio=true;
-         btnPedir.disabled =true;
-         btnDetener.disabled =true;
-         turnoCPU(puntosJugador);
-   
-         
-    }else if(puntosJugador===21){
-        console.log('21,genial');
-        btnPedir.disabled =true;
-        btnDetener.disabled =true;
-        turnoCPU(puntosJugador);
-     }
-
-
-}
-)
-
-btnDetener.addEventListener('click',function(){
-
-    turnoCPU(puntosJugador);
-    btnPedir.disabled    =true;
-    btnDetener.disabled  =true;
-
-})
-
- const isWiner=(resultado=>{
-    setTimeout(() => {
-        resultado=(puntosJugador===puntosCPU)
-        ?alert('Empate')
-        :((puntosJugador>21))
-        ?alert('Perdiste')
-        : (puntosCPU>21)
-        ?alert('ganaste')
-        :(puntosJugador>puntosCPU)
-        ?alert('ganaste')
-        :alert("perdiste")
-        console.log(resultado);
+    //! Eventos 
+    // usamos el listener para escuchar al boton y hacemos un callback al mandar una funcion por el argumento 
+    btnPedir.addEventListener('click',function(){
         
-    }, 10);   
+        const carta=pedirCarta();
+        
+        let  puntosJugador=  acumularPuntos(0,carta)
+        console.log({puntosJugador});
 
- })
+        crearCarta(carta,0);
 
-
- btnNuevo.addEventListener('click',()=>{
-
-
-    deck=  [];
-    deck=  crearDeck();
-    puntosJugador=0;
-    puntosCPU=0;
-
-    smallPuntos[0].innerText=0;
-    smallPuntos[1].innerText=0;
-
-    divCartaJugador.innerHTML='';
-    divCartaCPU.innerHTML='';
-
-    btnDetener.disabled=false;
-    btnPedir.disabled=false;
- })
+        if(puntosJugador>21){
+            btnPedir.disabled =true;
+            btnDetener.disabled =true;
+            turnoCPU(puntosJugador);
+    
+            
+        }else if(puntosJugador===21){
+            btnPedir.disabled =true;
+            btnDetener.disabled =true;
+            turnoCPU(puntosJugador);
+        }
 
 
+    }
+    )
+
+    btnDetener.addEventListener('click',function(){
+        console.log(puntosJugadores[0]);
+        turnoCPU(puntosJugadores[0]);
+        btnPedir.disabled    =true;
+        btnDetener.disabled  =true;
+
+    })
+
+    const isWiner=(resultado=>{
+        const [puntosJugador,puntosCPU]=puntosJugadores;
+
+        setTimeout(() => {
+            resultado=(puntosJugador===puntosCPU)
+            ?alert('Empate')
+            :((puntosJugador>21))
+            ?alert('Perdiste')
+            : (puntosCPU>21)
+            ?alert('ganaste')
+            :(puntosJugador>puntosCPU)
+            ?alert('ganaste')
+            :alert("perdiste")
+            console.log(resultado);
+            
+        }, 100);   
+
+    })
+
+
+    btnNuevo.addEventListener('click',()=>{
+
+        inicializarJuego();
+ 
+    })
+
+    return {
+        nuevoJuego:inicializarJuego
+    };
+})();
 
